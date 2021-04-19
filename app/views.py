7cd035693af702fb7dd51a9d460a84bb6205d425
Danlon Situma <situmadanlon@gmail.com>
2021-04-19 15:26:14 +0300

    Imported get article function from request mod

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template
 from app import app
+from .request import get_articles
 
 #Views
 @app.route('/')
@@ -7,5 +8,8 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    # getting us news
+    us_articles = get_articles('us')
+    print(us_articles)
     title = 'Habari /Home of up to date news.'
-    return render_template('index.html')
+    return render_template('index.html',title=title,us = us_articles)
