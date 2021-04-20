c7347cdbe8f6a4a52a1122913d67ebdaf7d1dfb7
Danlon Situma <situmadanlon@gmail.com>
2021-04-20 15:36:09 +0300

    Add template file

--- app/__init__.py
+++ app/__init__.py
@@ -11,4 +11,12 @@ app.config.from_pyfile('config.py')
 
 #Initializing Flask extensions
 bootstrap = Bootstrap(app)
+
+@app.template_filter()
+def format_datetime(value, format='medium'):
+    if format == 'full':
+        format =="EEEE, d. MMMM y 'at' HH:mm"
+    elif format == 'medium':
+        format = "EE dd.MM.y HH:mm"
+    return babel.dates.format_datetime(value, format)
 from app import views
