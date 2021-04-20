da7fe4d959e8969e30c91743c3777f99b9d71d65
Danlon Situma <situmadanlon@gmail.com>
2021-04-20 15:29:49 +0300

    Add source route decorator

--- app/views.py
+++ app/views.py
@@ -12,3 +12,11 @@ def index():
     all_sources = get_sources()
     title = 'Habari /Home of up to date news.'
     return render_template('index.html',title=title,sources = all_sources)
+
+@app.route('/source/<string:id>')
+def source(id):
+    '''
+    View aticles from one source function
+    '''
+    source = get_articles(id)
+    return render_template('source.html',  source=source)
